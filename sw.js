9132b9508d0355aec5a2e9ebbfa867c0bb451027
forrendo1234-dev <forrendo1234@gmail.com>
2026-07-07 18:59:35 -0300

    Proyecto 2000 v1: PWA instalable con modo offline

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'proyecto2000-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request, { ignoreSearch: true }).then(r => r || caches.match('./index.html')))
+  );
+});
